ea6cc35b224aa9c7b07f3d24a5f6dcc93ce9aca2
Jason Huang <cjhuang@yahoo-inc.com>
2016-04-15 17:30:05 +0800

    add eslint

--- test/app-test.js
+++ test/app-test.js
@@ -1,8 +1,10 @@
+/* global describe, it */
+
 import expect from 'expect';
 
 describe('suite', () => {
   it('should pass the dummy test', done => {
     expect(true).toEqual(true, 'Gotcha');
     done();
-  })
+  });
 });
